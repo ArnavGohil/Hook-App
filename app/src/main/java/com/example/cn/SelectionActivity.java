9583b23d7515f792a5e2c12ca83343a7e4b6714f
Arnav <arnav.gohil04@gmail.com>
2021-04-14 00:57:58 +0530

    Selection Screen Done :tada:

--- app/src/main/java/com/example/cn/SelectionActivity.java
+++ app/src/main/java/com/example/cn/SelectionActivity.java
@@ -1,12 +1,14 @@
 package com.example.cn;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import soup.neumorphism.NeumorphCardView;
 import soup.neumorphism.NeumorphFloatingActionButton;
 
 public class SelectionActivity extends AppCompatActivity {
@@ -21,5 +23,13 @@ public class SelectionActivity extends AppCompatActivity {
             ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SelectionActivity.this);
             startActivity(new Intent(SelectionActivity.this, SharingActivity.class), options.toBundle());
         });
+    }
+
+    public void select(View view) {
+        NeumorphCardView card = (NeumorphCardView) view;
+        if (card.getStrokeColor() == getColorStateList(R.color.selection))
+            card.setStrokeColor(getColorStateList(R.color.trans));
+        else
+            card.setStrokeColor(getColorStateList(R.color.selection));
     }
 }
