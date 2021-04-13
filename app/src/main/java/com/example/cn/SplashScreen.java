e27256c252f4fb661084744d43f2f02c6e0dde67
Arnav <arnav.gohil04@gmail.com>
2021-04-13 15:23:51 +0530

    Splash Screen Ready :tada:

--- app/src/main/java/com/example/cn/SplashScreen.java
+++ app/src/main/java/com/example/cn/SplashScreen.java
@@ -2,7 +2,14 @@ package com.example.cn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +17,10 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this);
+            startActivity(new Intent(SplashScreen.this, MainActivity.class), options.toBundle());
+        },1750);
     }
 }
