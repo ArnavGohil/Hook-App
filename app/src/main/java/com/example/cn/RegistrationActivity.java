8255f3892d7bff6998d94edc77cfaf143e2c196a
Arnav <arnav.gohil04@gmail.com>
2021-04-14 02:18:13 +0530

    Registration Screen :feet:

--- app/src/main/java/com/example/cn/RegistrationActivity.java
+++ app/src/main/java/com/example/cn/RegistrationActivity.java
@@ -3,6 +3,10 @@ package com.example.cn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        TextInputLayout phone = findViewById(R.id.phone);
+//        Toast.makeText(this, phone.getEndIconDrawable().toString(), Toast.LENGTH_SHORT).show();
+
     }
 }
