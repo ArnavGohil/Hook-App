665d4fc9a2a7d1be66f63c19b2dce93caa51f831
Arnav <arnav.gohil04@gmail.com>
2021-04-13 18:11:03 +0530

    Selection Screen :feet:

--- app/src/main/java/com/example/cn/MainActivity.java
+++ app/src/main/java/com/example/cn/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.cn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import soup.neumorphism.NeumorphButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NeumorphButton login = findViewById(R.id.login) ;
+        ImageView photo = findViewById(R.id.userPhoto) ;
+        TextView name = findViewById(R.id.userName);
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
+
+        photo.setOnClickListener(view -> {
+            // TODO
+            startActivity(new Intent(MainActivity.this, RegistrationActivity.class), options.toBundle());
+        });
+
+        login.setOnClickListener(view -> {
+            // TODO
+            startActivity(new Intent(MainActivity.this, SelectionActivity.class), options.toBundle());
+        });
+
     }
 }
