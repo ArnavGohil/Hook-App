2e7cb257e7ec1c7ff27847befbde447b2253fb17
Arnav <arnav.gohil04@gmail.com>
2021-04-30 13:38:43 +0530

    NFC in Progress :feet:

--- app/src/main/java/com/example/cn/SharingActivity.java
+++ app/src/main/java/com/example/cn/SharingActivity.java
@@ -1,9 +1,15 @@
 package com.example.cn;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,9 +23,15 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import soup.neumorphism.NeumorphButton;
 
 public class SharingActivity extends AppCompatActivity {
+
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +78,8 @@ public class SharingActivity extends AppCompatActivity {
                     .setBackgroundTint(Color.parseColor("#344955"))
                     .show();
             return;
-        }
+        } else
+            nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
 /*
         new Handler().postDelayed(() -> {
@@ -77,8 +90,65 @@ public class SharingActivity extends AppCompatActivity {
         }, 2500);
 */
 
-        // TODO Asli kaam ab shuru
+        // TODO Turn off NFC receiving
+
+        Context context = this;
+        Timer t = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(context, CardService.class);
+                intent.putExtra("ndefMessage", data);
+                startService(intent);
+            }
+
+        };
+
+//        t.scheduleAtFixedRate(task, 0, 10000);
+
+        Intent intent = new Intent(context, CardService.class);
+        intent.putExtra("ndefMessage", data);
+        startService(intent);
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (nfcAdapter != null) {
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        }
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        resolveIntent(intent);
+    }
 
 
+    private void resolveIntent(Intent intent) {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction()) || NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction()) || NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Log.e("HostApduService", print(tag.getTechList()));
+        }
+    }
+
+    private String print(String[] techList) {
+        StringBuilder str = new StringBuilder();
+        for (String s : techList)
+            str.append(s).append("\n");
+        return str.toString();
     }
 }
